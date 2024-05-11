cb6539c1feba630fc63fd6a46a1ebc337837b07d
Sprayingjosh <joshuadamilare3@gmail.com>
2024-05-11 14:11:23 +0100

    new assignment

---
+++ Objects.js
@@ -0,0 +1,16 @@
+/*
+let person = {
+    firstname: "John",
+    lastname: "Mote",
+    age:100,
+    eyeColor: "purple"
+};
+const x = person;
+x.age = 1000; //will change both x.age and person.age
+console.log(x.age); */
+
+let i = 0
+while (i < 10) {
+    console.log ("The number is " + i);
+    i ++;
+}
